318a0ef99d2b77065513293042f93cb31388b60e
Harshit Kumar <harshitkumar.hk@outlook.com>
2019-01-28 19:10:12 -0600

    Test Case 1 : checkAValidISBN() to fail first test case.

--- code/test/com/tdd/acmisbnvalidator/ValidateISBNTest.java
+++ code/test/com/tdd/acmisbnvalidator/ValidateISBNTest.java
@@ -1,17 +1,13 @@
 package com.tdd.acmisbnvalidator;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class ValidateISBNTest {
 
-	/**
-	 * Testing green condition
-	 */
 	@Test
-	public void test() {
-		//fail("Not yet implemented");
+	public void checkAValidISBN() {
+		fail();
 	}
 
 }
