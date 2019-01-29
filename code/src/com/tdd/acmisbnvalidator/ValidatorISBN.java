350272ed4f55b9b4b0fa14ea12feaaa637c210b8
Harshit Kumar <harshitkumar.hk@outlook.com>
2019-01-28 19:25:50 -0600

    Added: Red stage , failed checkISBN for 10 digit ISBN

--- code/src/com/tdd/acmisbnvalidator/ValidatorISBN.java
+++ code/src/com/tdd/acmisbnvalidator/ValidatorISBN.java
@@ -2,4 +2,9 @@ package com.tdd.acmisbnvalidator;
 
 public class ValidatorISBN {
 
+	public boolean checkISBN(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
--- code/test/com/tdd/acmisbnvalidator/ValidateISBNTest.java
+++ code/test/com/tdd/acmisbnvalidator/ValidateISBNTest.java
@@ -4,10 +4,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ValidateISBNTest {
-
 	@Test
 	public void checkAValidISBN() {
-		fail();
+		ValidatorISBN validator = new ValidatorISBN();	
+		boolean isValidISBN = validator.checkISBN(201847051);
+		assertTrue(isValidISBN);
 	}
 
 }
