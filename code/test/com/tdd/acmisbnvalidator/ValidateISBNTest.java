d21bb04444c757672efeda351797967e74dc183e
Harshit Kumar <harshitkumar.hk@outlook.com>
2019-01-28 19:55:57 -0600

    Added: two ISBN check in checkAValidISBN function

--- code/test/com/tdd/acmisbnvalidator/ValidateISBNTest.java
+++ code/test/com/tdd/acmisbnvalidator/ValidateISBNTest.java
@@ -8,7 +8,9 @@ public class ValidateISBNTest {
 	public void checkAValidISBN() {
 		ValidatorISBN validator = new ValidatorISBN();	
 		boolean isValidISBN = validator.checkISBN(201847051);
-		assertTrue(isValidISBN);
+		assertTrue("First Publication",isValidISBN);
+		isValidISBN = validator.checkISBN(387858199);
+		assertTrue("Second Publication",isValidISBN);
 	}
 	@Test
 	public void checkAnInValidISBN() {
